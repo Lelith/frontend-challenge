5a778eb207bd76d6e5b618d751f33059db4fee7b
Kathrin Holzmann <lelith@gmx.de>
2018-08-15 09:31:07 +0200

    feat(Routing): setup basic routes

---
+++ src/container/Calendar_Page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CalendarPage = () => (
+  <div className="pageContent calendarPage">
+    <h1>Calendar</h1>
+  </div>
+);
+
+export default CalendarPage;
---
+++ src/container/Success_Page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SuccessPage = () => (
+  <div className="pageContent">
+    <h1>Success</h1>
+  </div>
+);
+
+export default SuccessPage;
