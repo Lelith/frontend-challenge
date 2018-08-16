744f9456bb6a2fbea459d79034fd4ef40fac2b5c
Kathrin Holzmann <lelith@gmx.de>
2018-08-16 17:12:48 +0200

    style(Homepage): refactor and polish homepage

--- src/container/Home_Page.js
+++ src/container/Home_Page.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Button } from '../components/FormElements';
+import HomepageImg from '../img/homepage.jpg';
+
+require('./HomePage.css');
 
 const HomePage = () => (
-  <div className="pageContent">
-    <h1>Home</h1>
-    <Button as="a" href="/calendar" label="get started" />
+  <div className="homepage">
+    <img className="homepage__img" src={HomepageImg} alt="fashion for men" />
+    <div className="homepage__content">
+      <h1 className="homepage__title">Shopping for men</h1>
+      <span className="homepage__subtitle">Your personal style expert will create individual Outfits for you</span>
+      <Button className="homepage__button" as="a" href="/calendar" label="get started" />
+    </div>
   </div>
 );
 
